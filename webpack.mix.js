b0b01b2b65b8d1e14e66709d9993d2457f39b369
firmanramdhani <framdhani01@gmail.com>
2019-07-15 17:34:36 +0700

    setup

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,6 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+mix.js('resources/js/admin/admin.js', 'public/js/admin')
+    .sass('resources/sass/admin.scss', 'public/css/admin');
