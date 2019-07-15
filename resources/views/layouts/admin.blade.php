56696dced503f02caf6c7713089c1a5a5124a5e9
firmanramdhani <framdhani01@gmail.com>
2019-07-15 17:45:27 +0700

    update

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,13 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/admin/admin.js') }}" defer></script>
+
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/admin/admin.css') }}">
+
+
+
 </head>
+
 <body>
-    
+    <main>
+        @yield('content')
+    </main>
 </body>
+
 </html>
