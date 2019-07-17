f2fa3eb5699cdf3508a40099d9d85b53ea122e8a
Velia Sagita <velia.sagita.tif17@polban.ac.id>
2019-07-17 15:04:20 +0700

    edit create booking

--- app/Http/Controllers/API/BookingController.php
+++ app/Http/Controllers/API/BookingController.php
@@ -15,8 +15,7 @@ class BookingController extends Controller
     // //input booking baik dari user maupun client
     public function createBooking(request $request){
         $booking = new bookings();
-        // $booking->id_booking = (String) Str::uuid();
-        $booking->id_booking = $request->id_booking;
+        $booking->id_booking = (String) Str::uuid();
         $booking->id_schedule = $request->id_schedule;
         $booking->client_email = $request->client_email;
         $booking->user_email = $request->user_email;
@@ -34,12 +33,12 @@ class BookingController extends Controller
         return $listBooking;
     }
     //untuk mengambil data booking berdasarkan lokasi dan tanggal untuk update status
-    // public function getBookingByDate($location, $date){
-    public function getBookingByDate($date){
+    public function getBookingByDate($location, $date){
         // $userID = locations::where('id_location',$location)->get('id_user');
         // $email =  users::where('id_users',$userID)->get('email');
         // $listBooking = bookings::where('client_email',$email)->where('created_at',$date)->get();
         $listBooking = bookings::where('created_at',$date)->get();
         return $listBooking;
     }
+    //edit     
 }
