b9536f91d0c253f5896690c8e134df824313d6a6
dava87 <mochamaddavaaa@gmail.com>
2019-07-18 11:33:06 +0700

    adduuid

--- routes/api.php
+++ routes/api.php
@@ -17,14 +17,12 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-<<<<<<< HEAD
 
 Route::get('field','FieldController@index');
 Route::post('field/add','FieldController@create');
 Route::get('field/show','FieldController@show');
 Route::get('field/getfieldlocation/{id_location}','FieldController@getLapangLocation');
 // Route::get('field/getfieldid/{id_location}','FieldController@getIdField');
-=======
 Route::get('location','API\LocationsController@getLocation');
 Route::post('location','API\LocationsController@create');
 // Route::get('/location/{idClient}','API\LocationsController@getLocationClient');
@@ -34,4 +32,3 @@ Route::get('/location/delete/{idLocation}','API\LocationsController@deleteLocati
 
 Route::get('client','API\UsersController@getClient');
 Route::get('/client/{email}','API\UsersController@getIdClient');
->>>>>>> a296996a50d9fe460502867e8163a1a236a40f85
