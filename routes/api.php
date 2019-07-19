3155b34da2b26467230e0a38ed0cd6e0e823dc65
DhandyFr <dhandy.friyadi1@gmail.com>
2019-07-19 09:12:40 +0700

    add api_token to users table

--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,23 @@ Route::get('/location/{emailClient}','API\LocationsController@getLocationClient'
 Route::get('/locations/{city}','API\LocationsController@searchLocation');
 Route::delete('/location/delete/{idLocation}','API\LocationsController@deleteLocation');
 
+//Route untuk admin
+Route::get('admin','API\UsersController@getAdmin');
+Route::get('/admin/search/{name}','API\UsersController@searchAdmin');
+Route::put('/admin/update/{idAdmin}','API\UsersController@updateAdmin');
+Route::delete('/admin/delete/{email}','API\UsersController@deleteAdmin');
+
+//Route untuk client
 Route::get('client','API\UsersController@getClient');
-Route::get('/client/{email}','API\UsersController@getIdClient');
+Route::get('/client/search/{name}','API\UsersController@searchClient');
 Route::delete('/client/delete/{email}','API\UsersController@deleteClient');
+
+//Route untuk user
+Route::post('user/register','API\UsersController@createUser');
+Route::get('user','API\UsersController@getUser');
+Route::get('user/search/{name}','API\UsersController@searchUser');
+Route::delete('/user/delete/{email}','API\UsersController@deleteUser');
+
+Route::get('all','API\UsersController@getAllUser');
+
+Route::get('all/{name}','API\UsersController@searchAllUser');
