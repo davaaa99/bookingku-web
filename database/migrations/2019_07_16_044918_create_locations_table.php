a1aa3d4c86a3baaeff9c7480bca3cc00bc53feef
Velia Sagita <velia.sagita.tif17@polban.ac.id>
2019-07-29 14:11:52 +0700

    edit location migration

--- database/migrations/2019_07_16_044918_create_locations_table.php
+++ database/migrations/2019_07_16_044918_create_locations_table.php
@@ -15,7 +15,7 @@ class CreateLocationsTable extends Migration {
 		Schema::create('locations', function(Blueprint $table)
 		{
 			$table->string('id_location', 50)->primary();
-			$table->string('id_users', 50)->nullable()->index('fk_users_location');
+			$table->string('id_user', 50)->nullable()->index('fk_users_location');
 			$table->string('location_name', 50)->nullable();
 			$table->string('location_address')->nullable();
 			$table->text('description', 65535)->nullable();
