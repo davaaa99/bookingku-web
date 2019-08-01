4e74831b06593c680926c41b66117cbcdd8b710b
dava87 <mochamaddavaaa@gmail.com>
2019-08-01 13:37:11 +0700

    add menulapang

--- resources/js/client/components/menuLapanganComponent.vue
+++ resources/js/client/components/menuLapanganComponent.vue
@@ -1,41 +1,124 @@
 <template>
-<div id="location select">
-    <b-form-select v-model="selected" :options="options"></b-form-select>
-    <b-form-select v-model="selected" :options="options" size="sm" class="mt-3"></b-form-select>
-    <div class="mt-3">Selected: <strong>{{ selected }}</strong></div>
-
-  <!-- <div id="detail-lapangan">
-    <div class="lapangan" v-for="lapangan in detailLapangan" :key="lapangan.detailLapangan">
-      <div class="col-md-12 col-lg-6 col-sm-12">
-        <div class="image" :style="{ 'background-image': 'url(' + lapangan.img + ')' }"></div>
-      </div>
-      <div class="col-md-12 col-lg-6">
-        <div class="nama-lapangan">{{lapangan.namaLapangan}}</div>
-      </div>
-    </div> -->
+<div id="content">
+  <div id="list">
+        <div class="filter ">
+            <div class="filter-item d-flex">
+                <div class="filterStatus">
+                    <div class="filterTitle d-flex align-items-center " @click="show">
+                        {{filterStatus}}
+                        <i :class="[{ 'fas fa-caret-down':!status }, { 'fas fa-sort-up':status }]" class="ml-auto"></i>
+                    </div>
+                    <div v-if="status" class="box d-flex align-items-center justify-content-center">
+                        <div class="link">
+                            <div class="link-item" v-for="list in statusList" :key="list.statusList">
+                                <a @click="click(list.status)">
+                                    <div class="link-status">
+                                        {{ list.status }}
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="buttonadd">
+                  <b-button variant="primary">Primary</b-button>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+   
+    
+    <div class ="cardlapang" v-for="lapangan in dataLapangan" :key="lapangan.dataLapangan">
+        <b-card-group deck>
+      <b-card img-src= "http://www.staradmiral.com/wp-content/uploads/2017/01/Empat-Macam-Lapangan-Futsal.jpg" img-alt="Card image" img-top>
+        <b-card-title>
+          {{lapangan.namalapangan}}
+        </b-card-title>
+        <b-card-text>
+          {{lapangan.jenislapangan}}
+        </b-card-text>
+      </b-card>
+        
+        </b-card-group>
+        
+    </div>
 </div>
 </template>
 
 <script>
-import DatePicker from "vue2-datepicker";
-export default {
-  props: {
-    idLokasi: {
-      type: String,
-      required: true
-    }
-  },
-  data() {
-    return {
-      selected: null,
-        options: [
-          { value: null, text: 'Please select an option' },
-          { value: 'a', text: 'This is First option' },
-          { value: 'b', text: 'Selected Option' },
-          { value: { C: '3PO' }, text: 'This is an option with object value' },
-          { value: 'd', text: 'This one is disabled', disabled: true }
-        ]
+    import DatePicker from 'vue2-datepicker'
+    import BootstrapVue from 'bootstrap-vue'
+    import {
+        type
+    } from 'os';
+    Vue.use(BootstrapVue)
+
+    export default {
+        components: {
+            DatePicker
+        },
+        props: {
+            idBooking: [String]
+        },
+        data() {
+            return {
+                status: false,
+                perPage: 5,
+                currentPage: 1,
+                filterTanggal: "",
+                filterStatus: "Filter Status",
+                filterSearch: "",
+                statusList: [{
+                        id: '001',
+                        status: 'Waiting'
+                    },
+                    {
+                        id: '002',
+                        status: 'Confirmed'
+                    },
+                    {
+                        id: '003',
+                        status: 'Rejected'
+                    }
+                ],
+                dataLapangan: [{
+                  namalapangan : 'Lapang A',
+                  jenislapangan: 'Sintetis',
+                  image: 'http://www.staradmiral.com/wp-content/uploads/2017/01/Empat-Macam-Lapangan-Futsal.jpg'
+                },
+                {
+                  namalapangan : 'Lapang B',
+                  jenislapangan: 'Vinyl',
+                  image: 'https://djsport.id/wp-content/uploads/2018/11/futsal-stadium-1024x576.jpg'
+                }
+                ]
+                
+            };
+        },
+        mounted() {
+            this.getData()
+        },
+        methods: {
+            
+            show: function () {
+                this.status = !this.status;
+            },
+            click: function (status) {
+                this.status = !this.status;
+                this.filterStatus = status;
+            },
+        },
+        watch: {
+            filterStatus: function (fal) {
+                var self = this;
+                const data = self.filterData.filter(function (project) {
+                    return project.status === fal;
+                });
+                self.filterData = data;
+            }
+        },
+      
     };
-  }
-};
+
 </script>
