600ccaed6a1beb90a37dda39efaa8992c92a6484
DhandyFr <dhandy.friyadi1@gmail.com>
2019-08-12 18:15:36 +0700

    merge

--- resources/views/layouts/client.blade.php
+++ resources/views/layouts/client.blade.php
@@ -26,7 +26,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round|Open+Sans">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
@@ -40,7 +40,7 @@
 </head>
 <body>
     <div id="app">
-        @include('layouts.clientSidebar')
+        @include('layouts.sidebarclient')
 
         <main>
             <div class="jumbotron jumbotron-fluid"></div>
